769469eb854e16acf67fb51cab0092ab5165a18c
edoardoMalaspina <e.malaspina@studenti.unipi.it>
2025-11-25 19:12:54 +0100

    Modified training scripts to read parameters from config.yaml

--- starting_kit/train2.py
+++ starting_kit/train2.py
@@ -4,9 +4,30 @@ import hydra
 
 @hydra.main(config_path='config', config_name='config', version_base="1.3")
 def main(args):
+
     model = YOLO(args.modelCheckpoint)
-    model.train(data="data/data2.yaml", epochs=20, imgsz=160, batch=64, lr0=0.0001, project="runs_yolo", close_mosaic=0, device='0')
-    res = model.val(data="data/data2.yaml", save_json=True, plots=True, project="runs_yolo", device='0')
+
+    # Train
+    model.train(
+        data=f"{args.dataDir}/data2.yaml",
+        epochs=args.epochs,
+        imgsz=args.imgSize,
+        batch=args.batchSize,
+        lr0=args.lr,
+        patience=args.patience,
+        project="runs_yolo",    
+        close_mosaic=0,
+        device='0'
+    )
+
+    # Validation
+    res = model.val(
+        data=f"{args.dataDir}/data2.yaml",
+        save_json=True,
+        plots=True,
+        project="runs_yolo",
+        device='0'
+    )
 
 if __name__ == "__main__":
     main()
