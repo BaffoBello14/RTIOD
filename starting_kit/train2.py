d1275793db6eed8d6a36b7d48ae139c0b6e03035
BaffoBello14 <giuliobello12@gmail.com>
2025-11-25 19:01:50 +0100

    Update training scripts to include project and device parameters for model training and validation

--- starting_kit/train2.py
+++ starting_kit/train2.py
@@ -5,8 +5,8 @@ import hydra
 @hydra.main(config_path='config', config_name='config', version_base="1.3")
 def main(args):
     model = YOLO(args.modelCheckpoint)
-    model.train(data="data/data2.yaml", epochs=20, imgsz=160, batch=64, lr0=0.0001, close_mosaic=0)
-    res = model.val(data="data/data2.yaml", save_json=True, plots=True)
+    model.train(data="data/data2.yaml", epochs=20, imgsz=160, batch=64, lr0=0.0001, project="runs_yolo", close_mosaic=0, device='0')
+    res = model.val(data="data/data2.yaml", save_json=True, plots=True, project="runs_yolo", device='0')
 
 if __name__ == "__main__":
     main()
